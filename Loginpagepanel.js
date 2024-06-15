c8251eb541fe791c320a7a08861bce6b84f90f3e
efrossey <38024920+efrossey@users.noreply.github.com>
2024-06-15 21:13:50 +0700

    Add files via upload

---
+++ Loginpagepanel.js
@@ -0,0 +1,213 @@
+export default {
+  async fetch(request) {
+    const html = `<!doctype html>
+<html lang="en">
+
+<head>
+    <title id="title"></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="theme-color" content="#3B5998" />
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;" />
+   <style>
+        body {
+  font-family: -apple-system, BlinkMacSystemFont, "segoe ui", Verdana, Roboto, "helvetica neue", Arial, sans-serif, "apple color emoji";
+  font-size: 14px; 
+  margin: 0;
+}
+
+@font-face {
+  font-family: 'fontello';
+  src: url('./font/fontello.eot?78273339');
+  src: url('./font/fontello.eot?78273339#iefix') format('embedded-opentype'),
+       url('./font/fontello.woff?78273339') format('woff'),
+       url('./font/fontello.ttf?78273339') format('truetype'),
+       url('./font/fontello.svg?78273339#fontello') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+iframe  {
+  float:left; 
+  height:20px; 
+  width:100%;
+}
+.main{
+  background:rgba(0, 0, 0, 0.5);
+  max-width: 350px;
+  height: 100%;
+  border-radius:5px;
+  border-top: 10px solid #00D69F;
+  border-bottom: 10px solid #00D69F;
+  border-left: 5px solid #00D69F;
+  border-right: 5px solid #00D69F;
+  padding: 10px;
+  margin-top: 10%;
+  margin-left:auto;
+  margin-right:auto;
+  transition: 0.3s;
+  text-align:center;
+  min-height:350px;
+  animation: fadein 1s;
+  -moz-animation: fadein 1s; /* Firefox */
+  -webkit-animation: fadein 1s; /* Safari and Chrome */
+  -o-animation: fadein 1s; /* Opera */
+}
+
+.box{
+  margin-bottom:5px;
+  text-align:center;
+}
+
+.brand{
+  color:#000000;
+  padding:0px;
+  margin:10px;
+  font-size:35px;
+  border-bottom: solid 2px #000;
+}
+
+.button2 {
+  background-color: #000;
+  background-image: linear-gradient(#00D69F, #00D69F, #00D69F);
+  border: none;
+  border-radius:10px;
+  color: red;
+  font-weight: bold;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  width: 100%;
+  height: 40px;
+}
+.button2:hover{
+  font-weight:bold;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.1), 0 2px 2px 0 rgba(0, 0, 0, 0.1);
+}
+
+.small-button {
+  background-color: #00D69F;
+  background-image: linear-gradient(#00D69F, #00D69F, #00D69F);
+  border: none;
+  border-radius: 10px;
+  color: red;
+  font-weight: bold;
+  text-align: center;
+  vertical-align: middle;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+  cursor: pointer;
+  width: 40%;
+  height: 44px;
+}
+
+.table {
+  width: 100%;
+  border-collapse: collapse !important;
+}
+
+.blink { 
+  font-size: 50px;
+  animation: blinker 0.75s linear infinite; } 
+@keyframes blinker { 
+    50% { opacity: 0; } 
+    
+}
+.blink2 { 
+  font-size: 20px;
+  animation: blinker 0.8s linear infinite; } 
+@keyframes blinker { 
+    50% { opacity: 0; } 
+    
+}
+
+
+@keyframes fadein {
+  from {opacity:0;}
+  to {opacity: 1;}
+}
+@-moz-keyframes fadein { /* Firefox */
+  from {opacity:0;}
+  to {opacity: 1;}
+}
+@-webkit-keyframes fadein { /* Safari and Chrome */
+  from {opacity:0;}
+  to {opacity: 1;}
+}
+@-o-keyframes fadein { /* Opera */
+  from {opacity:0;}
+  to {opacity: 1;}
+}
+
+/* @media */
+@media screen and (max-width: 600px) {
+  .main{
+	width:100%;
+  margin-top: 10%;
+}}
+
+/* @media */
+@media screen and (min-width: 600px) {
+  .main{
+	margin-top: 50%;
+}}
+
+        .frame img {
+            width: 100%;
+            height: auto;
+        }
+        body {
+            color: red;
+            font-family: Arial, sans-serif;
+            background-color: black;
+            background-size: cover;
+            background-position: auto;
+        }
+      </style>
+<body>
+            <div id="main" class="main">
+            <div class="box">
+            <div class="frame">
+            </div>
+            <br>
+                <table class="table">
+                  <caption style= 'font-size: 40px; font-weight: bold; margin-bottom:15px; color: #00D69F;'> FREE VLESS</caption>
+                  <caption style= 'font-size: 30px; font-weight: bold; margin-bottom:15px; color: #00D69F;'> CLOUDFLARE</caption>
+                  </table>
+                <br>
+            <div class="box">
+                <button class="button2" onclick=</i>🇮🇩 SERVER INDONESIA 🇮🇩</button>
+                <button class="small-button" onclick="window.location='https://xcl.aframdani.cloud/noir7r';"></i>VLESS ID V1</button>
+                <button class="small-button" onclick="window.location='https://biz-xcl.aframdani.cloud/noir7r';"></i>VLESS ID V2</button>     
+                <button class="button2" onclick=</i>🇸🇬 SERVER SINGAPORE 🇸🇬</button>
+                <button class="small-button" onclick="window.location='https://sgdo1.aframdani.cloud/noir7r';"></i>DIGITALOCEAN</button>
+                <button class="small-button" onclick="window.location='https://v2ray.aframdani.cloud/noir7r';"></i>OVH SAS</button>
+                <button class="small-button" onclick="window.location='https://sg1.aframdani.cloud/noir7r';"></i>TCC</button>
+                <button class="small-button" onclick="window.location='https://sg2.aframdani.cloud/noir7r';"></i>ALIBABA</button>  
+                <button class="small-button" onclick="window.location='https://aio.aframdani.cloud/noir7r';"></i>-----</button>
+                <button class="small-button" onclick="window.location='https://aio.aframdani.cloud/noir7r';"></i>-----</button>
+                <button class="button2" onclick="window.location='https://t.me/afrcloudd/251';">Note: Request Pointing Chat Me </i></button
+            </div>            
+            <br>
+            <br>
+            <br>
+            <div class='box' style='color:#00D69F;'><b><i><span class='blink2'>Powered By : AFR-Cloud</span></i></b></span></i></b>
+                 </div>       
+</body>
+</html>`;
+    return new Response(html, {
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    });
+  },
+};
